337d6bbb7e709cdebe8dad7b853a455e58431211
Ismaxlepro <ismail.khiel05@gmail.com>
2026-02-23 17:42:34 +0100

    Update links.js

--- templates/links.js
+++ templates/links.js
@@ -7,3 +7,15 @@ function clickwait() {
         content.classList.add("active");
     }
 }
+function wow() {
+   const image = document.getElementById("headerimage");
+   const header = document.querySelector("header");
+   if (image.classList.contains("active")) {
+        image.classList.remove("active");
+	header.classList.remove("active");
+    } else {
+        image.classList.add("active");
+        header.classList.add("active");
+    }
+
+}
